e7e3111a9a89003669fabdc632ec67b7fd7840d3
Jdasi <silvacore@hotmail.co.uk>
2017-09-25 15:38:02 +0100

    starting to add first person scripts

---
+++ Assets/Behaviours/FirstPersonMouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMouseLook : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] float horizontal_look_sensitivity = 150.0f;
+    [SerializeField] float vertical_look_sensitivity = 100.0f;
+    [SerializeField] bool y_flipped;
+
+    [Header("References")]
+    [SerializeField] Transform transform_to_rotate;
+
+    private float pan_horizontal;
+    private float pan_vertical;
+
+
+	void Start()
+    {
+        if (transform_to_rotate == null)
+            transform_to_rotate = this.transform;
+	}
+	
+
+	void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X");
+        float vertical = Input.GetAxis("Mouse Y");
+
+        pan_horizontal +=  horizontal * Time.deltaTime * horizontal_look_sensitivity;
+        pan_vertical -= (!y_flipped ? vertical : -vertical) * Time.deltaTime * vertical_look_sensitivity;
+
+        pan_vertical = Mathf.Clamp(pan_vertical, -90, 90);
+
+        transform_to_rotate.rotation = Quaternion.Euler(pan_vertical, pan_horizontal, 0);
+	}
+
+}
---
+++ Assets/Behaviours/FirstPersonMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    [Header("Paramters")]
+    [SerializeField] float speed = 30;
+    [SerializeField] float strafe_speed_modifier = 1;
+    [SerializeField] float back_speed_modifier = 1;
+    [SerializeField] bool allow_flying;
+
+    [Header("References")]
+    [SerializeField] Transform movement_relation;
+    
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed * strafe_speed_modifier;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        if (vertical < 0)
+            vertical *= back_speed_modifier;
+
+        if (movement_relation == null)
+        {
+            transform.position += new Vector3(horizontal, 0, vertical);
+        }
+        else
+        {
+            Vector3 move = (horizontal * movement_relation.transform.right) +
+                (vertical * movement_relation.transform.forward);
+
+            transform.position += move;
+        }
+    }
+
+}
