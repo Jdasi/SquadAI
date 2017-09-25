55041b4370c86aca1c585c14053a1947a63f40f7
Joe da Silva <silvacore@hotmail.co.uk>
2017-09-25 21:42:48 +0100

    added crouching

--- Assets/Behaviours/FirstPersonMovement.cs
+++ Assets/Behaviours/FirstPersonMovement.cs
@@ -9,7 +9,9 @@ public class FirstPersonMovement : MonoBehaviour
     [SerializeField] float noclip_speed = 15;
     [SerializeField] float strafe_speed_modifier = 1;
     [SerializeField] float back_speed_modifier = 1;
-    [SerializeField] float sprint_modifier = 1.5f;
+    [SerializeField] float sprint_speed_modifier = 1.5f;
+    [SerializeField] float crouch_speed_modifier = 0.5f;
+    [SerializeField] float crouch_shrink_factor = 0.5f;
     [SerializeField] float jump_force;
     [SerializeField] string player_layer;
     [SerializeField] string noclip_layer;
@@ -21,11 +23,13 @@ public class FirstPersonMovement : MonoBehaviour
     private float horizontal;
     private float vertical;
     private bool noclip;
+    private bool crouched;
     private bool sprinting;
     private bool grounded;
 
     private int player_layer_value;
     private int noclip_layer_value;
+    private Vector3 original_scale;
 
 
     public void ToggleNoclip()
@@ -41,6 +45,8 @@ public class FirstPersonMovement : MonoBehaviour
     {
         player_layer_value = LayerMask.NameToLayer(player_layer);
         noclip_layer_value = LayerMask.NameToLayer(noclip_layer);
+
+        original_scale = transform.localScale;
     }
 
 
@@ -52,8 +58,12 @@ public class FirstPersonMovement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * CurrentSpeed() * strafe_speed_modifier;
         vertical = Input.GetAxis("Vertical") * Time.deltaTime * CurrentSpeed();
 
-        sprinting = Input.GetButton("Sprint");
+        crouched = Input.GetButton("Crouch");
+        sprinting = !crouched && Input.GetButton("Sprint");
         grounded = Physics.Raycast(transform.position, Vector3.down, 1, ~player_layer_value);
+
+        transform.localScale = crouched ? new Vector3(original_scale.x,
+            original_scale.y * crouch_shrink_factor, original_scale.z) : original_scale;
 
         if (grounded && Input.GetButtonDown("Jump"))
         {
@@ -103,7 +113,8 @@ public class FirstPersonMovement : MonoBehaviour
     float CurrentSpeed()
     {
         return (noclip ? noclip_speed : speed) * (1 +
-            (sprinting ? sprint_modifier : 0));
+            (sprinting && !crouched ? sprint_speed_modifier : 0)) *
+            (crouched ? crouch_speed_modifier : 1);
     }
 
 }
